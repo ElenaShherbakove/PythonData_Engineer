b8cfb1d56820014f0614007bcd1ea63ad25de708
Elena <Shherbakove@yandex.ru>
2024-04-15 22:06:51 +0300

    Комментарий к 3 ДЗ

--- homework3/task2.py
+++ homework3/task2.py
@@ -1,12 +1,31 @@
 """
-Дан список повторяющихся элементов.
-Вернуть список с дублирующимися элементами.
-В результирующем списке не должно быть дубликатов.
+В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых.
+ Не учитывать знаки препинания и регистр символов.
+ За основу возьмите любую статью из википедии или из документации к языку.
 """
-lst = [1,2,3,4,5,6,8,8,8,9,7,6,5,4,3,2,1]
-new_set = set(lst) # получим множество уникальных элементов
-no_duble = []
-for i in new_set:
-    if lst.count(i) > 1: # если элемент уникального списка встречается  более 1 раза  добавляем  в список no_duble
-        no_duble.append(i)
-print(f'Список дублирующихся элементов: {no_duble}')
+from collections import Counter
+import re
+from os import path
+
+with open("text.txt", encoding="utf-8") as f:
+    text = f.read()
+text = re.sub(r"[^a-zA-Z]", " ", text)
+new_text = text.split()
+
+print(Counter(new_text).most_common()[:10])
+
+"""
+
+text = input("Введите текст")
+text = text.lower().replace(",", "").replace(".", "").replace("!", "").replace("?", "").replace("'"," ") # удаляем знаки препинания
+new_text = text.split()
+my_dict = {}
+for i in new_text:
+    if i.isdigit(): # если число
+       pass
+    else: 
+        my_dict[i] = my_dict.get(i, 0) + 1 
+sorted_dict = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
+
+print(sorted_dict[:10])
+"""
--- homework3/task3.py
+++ homework3/task3.py
@@ -1,31 +1,53 @@
 """
-В большой текстовой строке подсчитать количество встречаемых слов и вернуть 10 самых частых.
- Не учитывать знаки препинания и регистр символов.
- За основу возьмите любую статью из википедии или из документации к языку.
+4. Создайте словарь со списком вещей для похода в качестве ключа и их массой в качестве значения.
+ Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность.
+ Достаточно вернуть один допустимый вариант.*Верните все возможные варианты комплектации рюкзака.
 """
-from collections import Counter
-import re
-from os import path
+# *Верните все возможные варианты комплектации рюкзака.
 
-with open("text.txt", encoding="utf-8") as f:
-    text = f.read()
-text = re.sub(r"[^a-zA-Z]", " ", text)
-new_text = text.split()
+import random
 
-print(Counter(new_text).most_common()[:10])
+things = {
+    "спальник": 1.5,
+    "палатка": 3.2,
+    "термос": 0.6,
+    "карта": 0.1,
+    "фонарик": 0.3,
+    "котелок": 0.8,
+    "еда": 2.5,
+    "одежда": 1.7,
+    "обувь": 1.2,
+    "нож": 0.5
+}
 
+list_keys= [] # создадим лист из ключей
+for key in things.keys():
+    list_keys.append(key)
+
+max_weight = 7.0
+backpack = {}
+weight = 0
+
+while len(list_keys) > 0:  # пока лист из ключей не пустой, берем случайный предмет проверяем меньше ли он оставшегося веса
+    key_exit = random.choice(list_keys)
+    if max_weight - things[key_exit] >= 0:
+        max_weight -= things[key_exit]
+        backpack[key_exit] = things[key_exit] # закидываем рюкзак
+        weight += things[key_exit]
+    else: # иначе выкидываем
+        pass
+    list_keys.remove(key_exit) # в любом случае удаляем из листа ключей
+
+print(f'В рюкзак влезли {backpack} массой {weight}')
 """
+#  Достаточно вернуть один допустимый вариант
 
-text = input("Введите текст")
-text = text.lower().replace(",", "").replace(".", "").replace("!", "").replace("?", "").replace("'"," ") # удаляем знаки препинания
-new_text = text.split()
-my_dict = {}
-for i in new_text:
-    if i.isdigit(): # если число
-       pass
-    else: 
-        my_dict[i] = my_dict.get(i, 0) + 1 
-sorted_dict = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
-
-print(sorted_dict[:10])
+backpack = {}
+count = 0
+for k in things:
+   if max_weight - (count+things[k]) >= 0:
+        backpack[k] = things[k]
+        count += things[k]
+        print(count)
+print(backpack)
 """
