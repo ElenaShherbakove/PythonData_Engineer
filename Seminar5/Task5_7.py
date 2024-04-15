03fbbdeeb335716ac1beb7ce9442900593a10455
Elena <Shherbakove@yandex.ru>
2024-04-15 21:57:05 +0300

    Семинар5

---
+++ Seminar5/Task5_7.py
@@ -0,0 +1,25 @@
+"""Создайте функцию-генератор. 
+Функция генерирует N простых чисел, 
+начиная с числа 2. 
+Для проверки числа на простоту используйте правило: «число является простым, если делится нацело только на единицу и на себя»."""
+
+def prime(n):
+    if n<3:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def generate_simple():
+    n = 2
+    while True:
+        if prime(n):
+            yield n
+        n +=1
+
+j = generate_simple()
+for _ in range(int(input("введите число N: "))):
+    print(next(j))
