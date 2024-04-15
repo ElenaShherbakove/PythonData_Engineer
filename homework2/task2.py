aec6ca24e1ff6d58002868124c97f8fdb42df989
Elena <Shherbakove@yandex.ru>
2024-04-15 22:03:38 +0300

    Комментарий к 2 ДЗ

--- homework2/task2.py
+++ homework2/task2.py
@@ -1,30 +1,36 @@
 """
-Напишите программу,
- которая получает целое число и возвращает его шестнадцатеричное строковое представление.
- Функцию hex используйте для проверки своего результата.
+Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+ Программа должна возвращать сумму и произведение* дробей.
+   Для проверки своего кода используйте модуль fractions
 """
 
-def outnumber(number, base):
-    number_str = '0123456789ABCDEF'
-    result = ''
-    while number > base:
-        if base == 16:
-            result += number_str[number % base]
-        else:
-            result += str(number % base)    
-        number //= base
-    if base == 16:
-        if number  == base:
-           result += str("01")
-        else:    
-            result += number_str[number]
-    else:
-        result += str(number)
-    return result[::-1]
+from fractions import Fraction
+import re # для проверки строки на формат дроби
 
-number = int(input('Введите число: '))
-base = int(input('Введите систему исч 2/8/16: '))
-print(outnumber(number, base))
-print(hex(number))
-print(oct(number))
-print(bin(number))
+frac1 = input("введите дробь в формате (a/b): ")
+frac2 = input("введите дробь в формате (a/b): ")
+parts1 = re.split('/', frac1)
+parts2 = re.split('/', frac2)
+if len(parts1) != 2: # формат дроби а/б идем дальше только если 2 части
+  print("В первом случае введена не дробь")
+if len(parts2) != 2:
+  print("Во втором случае введена не дробь")
+else:
+  drob1 = frac1.split(sep ='/')
+  drob2 = frac2.split(sep ='/')
+  if (drob1[0].isdigit() and drob1[1].isdigit() and drob2[0].isdigit() and drob2[1].isdigit()):  #проверка на число в каждой части split
+    chisl = int(drob1[0]) * int(drob2[1]) + int(drob2[0]) * int(drob1[1])
+    znam = int(drob1[1]) * int(drob2[1])
+    rezult = str(chisl) + '/' + str(znam)
+    proizv = str(int(drob1[0])*int(drob2[0])) + '/' + str(int(drob1[1])*int(drob2[1]))
+    print(f'Сумма дробей: {rezult}')
+    print(f'Произведение дробей: {proizv}')
+  else:
+    print('в одной из строк числитель/ знаменатель не число!!!')
+
+## проверка с помощью функции Fraction
+x = Fraction(frac1)
+y = Fraction(frac2)
+print("Проверка функцией Fraction: ")
+print(f'Сумма дробей: {x+y}')
+print(f'Произведение дробей: {x*y}')
